8a3017fb7f9f09355b3810fd1d11488d8818b04b
Samgu Lee <cable8mm@gmail.com>
2026-06-07 01:34:55 +0900

    feat: publish manifesto, specification and workflow

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,5 @@
 import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 const baseCollectionSchema = z.object({
@@ -10,18 +11,38 @@ const baseCollectionSchema = z.object({
 
 export const collections = {
   manifesto: defineCollection({
+    loader: glob({
+      base: './src/content/manifesto',
+      pattern: '**/*.{md,mdx}',
+    }),
     schema: baseCollectionSchema,
   }),
   specification: defineCollection({
+    loader: glob({
+      base: './src/content/specification',
+      pattern: '**/*.{md,mdx}',
+    }),
     schema: baseCollectionSchema,
   }),
   workflow: defineCollection({
+    loader: glob({
+      base: './src/content/workflow',
+      pattern: '**/*.{md,mdx}',
+    }),
     schema: baseCollectionSchema,
   }),
   resources: defineCollection({
+    loader: glob({
+      base: './src/content/resources',
+      pattern: '**/*.{md,mdx}',
+    }),
     schema: baseCollectionSchema,
   }),
   showcase: defineCollection({
+    loader: glob({
+      base: './src/content/showcase',
+      pattern: '**/*.{md,mdx}',
+    }),
     schema: baseCollectionSchema,
   }),
 };
