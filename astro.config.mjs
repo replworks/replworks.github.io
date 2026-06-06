2679f76f98416df7e70079264660cb2573103949
Samgu Lee <cable8mm@gmail.com>
2026-06-06 20:09:15 +0900

    feat: configuration Github Pages Deployment(T005)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.repl.net",
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
