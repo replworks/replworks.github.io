a41f4f372044e0facdc07abe8c8441a329c20e72
Samgu Lee <cable8mm@gmail.com>
2026-06-08 15:01:41 +0900

    feat: add link test package for ci-cd

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import pagefind from 'astro-pagefind';
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
+import astroBrokenLinksChecker from 'astro-broken-links-checker';
 
 export default defineConfig({
   site: 'https://www.repl.net',
-  integrations: [mdx(), pagefind(), sitemap()],
+  integrations: [
+    mdx(),
+    pagefind(),
+    sitemap(),
+    astroBrokenLinksChecker({
+      throwError: true,
+    }),
+  ],
   vite: {
     // 빈 옵션 객체를 전달해 봅니다.
     plugins: [tailwindcss()],
