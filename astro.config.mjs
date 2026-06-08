456029b03ea2bd03d5b526effd0ac71ff2b9fa25
Samgu Lee <cable8mm@gmail.com>
2026-06-08 13:52:15 +0900

    chore: create `sitemap-index.xml` (#21)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import mdx from '@astrojs/mdx';
 import pagefind from 'astro-pagefind';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://www.repl.net',
-  integrations: [mdx(), pagefind()],
+  integrations: [mdx(), pagefind(), sitemap()],
   vite: {
     // 빈 옵션 객체를 전달해 봅니다.
     plugins: [tailwindcss()],
